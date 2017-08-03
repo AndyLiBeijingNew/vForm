cc672dc96a32ea1a5a56b5f4a7d9c6a42fb4fc1b
Ashish Sharma <asharma0@varian.com>
2017-08-03 22:53:51 +0530

    Merged changes with Simon's commit. AshishS

--- src/app/vform/components/dynamic/dynamic.component.ts
+++ src/app/vform/components/dynamic/dynamic.component.ts
@@ -1,4 +1,7 @@
-import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef, Input } from '@angular/core';
+import {
+  Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef, Input,
+  AfterContentInit
+} from '@angular/core';
 import { VFormComponentBase } from '../VFormComponentBase';
 import { HelperService } from '../../editors/property-editor/helper.service';
 import { Helper } from '../../helpers/Helper';
@@ -11,7 +14,7 @@ import { IListItemIndex } from '../IListItemIndex';
   templateUrl: './dynamic.component.html',
   host: { '[style.width]': 'metadata.properties.dynamicWidth || "100%"', '[style.height]': 'metadata.properties.dynamicHeight || "100%"', '[style.display]': '"flex"' }
 })
-export class DynamicComponent extends VFormComponentBase implements OnInit, IVFormContainerComponent {
+export class DynamicComponent extends VFormComponentBase implements OnInit, AfterContentInit, IVFormContainerComponent {
 
   @Input()
   itemIndex: IListItemIndex;
